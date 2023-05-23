b0161eec9c017ed5250fb774943e9dd99f31cea5
Hitalo-27 <hytalosantos26@gmail.com>
2023-05-22 23:13:24 -0300

    Fix

--- src/components/experiences/Experiences.js
+++ src/components/experiences/Experiences.js
@@ -8,6 +8,7 @@ import { TextDecrypt } from "../content/TextDecrypt";
 import './Experiences.css';
 
 import Evve from '../../assets/evve-logo.png';
+import Radar from '../../assets/radar.jpg';
 
 const useStyles = makeStyles((theme) => ({
   main: {
@@ -25,23 +26,43 @@ export const Exeperiences = () => {
     <section id="experiences">
       <Container component="main" className={classes.main} maxWidth="md">
 
+        <div className="experiencesDiv">
           <div className="project_experiences">
             <div className="__content_wrapper_experiences">
               <div className="divFlex">
                 <div className="__img_wrapper_experiences">
-                  <img src={ Evve } alt="unip"/>
+                  <img src={ Evve } alt="evve"/>
                 </div>
                 <div className="divDescription">
                   <h3 className="title_experiences">
                     <TextDecrypt text={ "Evve Comunicação" } />
                   </h3>
                   <p className="description_experiences">
-                    { "Atualmente estou trabalhando na evve comunicação desde setembro de 2022" }
+                    { "Atualmente estou estagiando na evve comunicação, desde setembro de 2022" }
                   </p>
                 </div>
               </div>
             </div>
           </div>
+
+          <div className="project_experiences">
+            <div className="__content_wrapper_experiences">
+              <div className="divFlex">
+                <div className="__img_wrapper_experiences">
+                  <img src={ Radar } alt="unip"/>
+                </div>
+                <div className="divDescription">
+                  <h3 className="title_experiences">
+                    <TextDecrypt text={ "Radar Consultas" } />
+                  </h3>
+                  <p className="description_experiences">
+                    { "Atualmente estou estagiando na radar consultas, desde abril de 2023" }
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
       </Container>
     </section>
